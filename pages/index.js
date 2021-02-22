138c1a8e5f032c8caf851192df42070875aa64b5
Karn Singh <gurkaran.12@gmail.com>
2021-02-22 06:21:59 -0500

    Addeed Merge sort

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,61 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from 'react'
+import mergeSort from '../sorts/mergesort';
+
 
 export default function Home() {
+  const [arr, setArr] = useState([]);
+ 
+  function generatearray(){
+    let arr1 = []
+
+    for(let i=20; i< 500; i+= 10){
+      arr1.push(Math.round(Math.random() * 500))
+    }
+    
+    setArr(arr1)
+
+    }
+
+    useEffect(() => {
+      generatearray()
+    },[]);
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Sorting Visualizer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.navbar}>
+          <button onClick={() => generatearray()}> Reset the array</button>
+          <button onClick={() => setArr(mergeSort(arr))}> MergeSort</button>
+          <button> Quick Sort</button>
+          <button> Bubble Sort</button>
+        </div>
+        <div className={styles.bars}>
+          {arr.map((x, id) => (<div className="bar" key={id}>
+              {x}
+              <style jsx>{`
+        .bar {
+          margin:2px;
+          height:200px;
+          border:1px solid black;
+          height: ${x}px;
+          background-color:   lightgray
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        }
+        
+      `}</style>
+          </div>))}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
       </footer>
     </div>
   )
